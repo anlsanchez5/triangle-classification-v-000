47598a4458a0428851156b87a1cf7d0ad5a936d1
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-16 23:42:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,15 @@ class Triangle
   end
 
   def kind
-    
-      raise TriangleError
-    
+    sum_12 = length_1 + length_2
+    sum_13 = length_1 + length_3
+    sum_23 = length_2 + length_3
+
+      raise TriangleError unless length_1 > 0 && length_2 > 0 && length_3 > 0 && sum_12 > length_3 && sum_23 > length_1 && sum_13 > length_2
+
+      
+
+
   end
 
   class TriangleError < StandardError
