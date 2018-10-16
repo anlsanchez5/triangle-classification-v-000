0d5a8c4caca32d12aa2f14cefe471d3d59756aef
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-16 23:37:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    
+      raise TriangleError
+    
+  end
+
+  class TriangleError < StandardError
+  end
 end
