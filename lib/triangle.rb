871c93f4fefffaa7e8152074e3e08153313cc00d
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-16 23:43:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,21 @@ class Triangle
     sum_13 = length_1 + length_3
     sum_23 = length_2 + length_3
 
-      raise TriangleError unless length_1 > 0 && length_2 > 0 && length_3 > 0 && sum_12 > length_3 && sum_23 > length_1 && sum_13 > length_2
+    raise TriangleError unless length_1 > 0 && length_2 > 0 && length_3 > 0 && sum_12 > length_3 && sum_23 > length_1 && sum_13 > length_2
 
-      
+    if length_1 == length_2 && length_1 == length_3
+      :equilateral
+    elsif length_1 == length_2 && length_1 != length_3
+      :isosceles
+    elsif length_2 == length_3 && length_2 != length_1
+      :isosceles
+    elsif length_1 == length_3 && length_1 != length_2
+      :isosceles
+    elsif length_1 != length_2 && length_1 != length_3 && length_2 != length_3
+      :scalene
+    end
+
+
 
 
   end
